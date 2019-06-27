a31df6f546fb1b0854082464ee741fad38d9acb4
NoemiRozpara <nrozpara@gmail.com>
2019-06-27 22:44:23 +0200

    comments

--- src/components/DraggableCart.js
+++ src/components/DraggableCart.js
@@ -6,12 +6,20 @@ import shoppingCart from '../assets/icons/shoppingCart.png';
 class DraggableCart extends React.Component {
   constructor(props) {
     super(props);
+    // init future transform value
     this.animatedValue = new Animated.ValueXY();
+
+    // init helper for keeping transform between gestures - without this component would jump on every new gesture
     this._value = { x: 0, y: 0 };
     this.animatedValue.addListener(value => (this._value = value));
+
+    // init pan responder
     this.panResponder = PanResponder.create({
+      // ask for permissions
       onStartShouldSetPanResponder: (evt, gestureState) => true,
       onMoveShouldSetPanResponder: (evt, gestureState) => true,
+
+      // on gesture start start counting from stored values
       onPanResponderGrant: (e, gestureState) => {
         this.animatedValue.setOffset({
           x: this._value.x,
@@ -19,6 +27,8 @@ class DraggableCart extends React.Component {
         });
         this.animatedValue.setValue({ x: 0, y: 0 });
       },
+
+      // update transform value using gesture movement delta
       onPanResponderMove: (event, gestureState) => {
         this.animatedValue.setValue({ x: gestureState.dx, y: gestureState.dy });
       },
@@ -29,12 +39,12 @@ class DraggableCart extends React.Component {
     const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
     const cartSize = { width: 70, height: 70 };
 
+    // don't allow component movement outside the screen
     const constrainedX = this.animatedValue.x.interpolate({
       inputRange: [-screenWidth + cartSize.width, 0],
       outputRange: [-screenWidth + cartSize.width, 0],
       extrapolate: 'clamp',
     });
-
     const constrainedY = this.animatedValue.y.interpolate({
       inputRange: [-screenHeight + cartSize.height, 0],
       outputRange: [-screenHeight + cartSize.height, 0],
