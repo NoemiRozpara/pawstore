beee5e3a9fad255ab2149c1f80715ea7a7d17d6d
NoemiRozpara <nrozpara@gmail.com>
2019-06-27 21:48:48 +0200

    animated

--- src/components/DraggableCart.js
+++ src/components/DraggableCart.js
@@ -1,13 +1,61 @@
 import React from 'react';
-import { StyleSheet, Image, View } from 'react-native';
+import { StyleSheet, Image, PanResponder, Dimensions, Animated } from 'react-native';
 
 import shoppingCart from '../assets/icons/shoppingCart.png';
 
-const DraggableCart = props => (
-  <View style={styles.box}>
-    <Image source={shoppingCart} style={{ width: 26, height: 26 }} />
-  </View>
-);
+class DraggableCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.animatedValue = new Animated.ValueXY();
+    this._value = { x: 0, y: 0 };
+    this.animatedValue.addListener(value => (this._value = value));
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderGrant: (e, gestureState) => {
+        this.animatedValue.setOffset({
+          x: this._value.x,
+          y: this._value.y,
+        });
+        this.animatedValue.setValue({ x: 0, y: 0 });
+      },
+      onPanResponderMove: (event, gestureState) => {
+        this.animatedValue.setValue({ x: gestureState.dx, y: gestureState.dy });
+      },
+    });
+  }
+
+  render() {
+    const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
+    const cartSize = { width: 70, height: 70 };
+
+    const constrainedX = this.animatedValue.x.interpolate({
+      inputRange: [-screenWidth + cartSize.width, 0],
+      outputRange: [-screenWidth + cartSize.width, 0],
+      extrapolate: 'clamp',
+    });
+
+    const constrainedY = this.animatedValue.y.interpolate({
+      inputRange: [-screenHeight + cartSize.height, 0],
+      outputRange: [-screenHeight + cartSize.height, 0],
+      extrapolate: 'clamp',
+    });
+
+    return (
+      <Animated.View
+        style={[
+          styles.box,
+          {
+            transform: [{ translateX: constrainedX }, { translateY: constrainedY }],
+          },
+        ]}
+        {...this.panResponder.panHandlers}
+      >
+        <Image source={shoppingCart} style={{ width: 26, height: 26 }} />
+      </Animated.View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   box: {
