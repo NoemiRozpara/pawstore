3170fed0b47fc67befa6cea8c9f41ba9f83ca035
NoemiRozpara <nrozpara@gmail.com>
2019-06-27 22:36:17 +0200

    reanimated - WIP

--- src/components/DraggableCart.js
+++ src/components/DraggableCart.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import { StyleSheet, Image, View } from 'react-native';
+import { PanGestureHandler, ScrollView, State } from 'react-native-gesture-handler';
+import { Animated } from 'react-native-reanimated';
 
 import shoppingCart from '../assets/icons/shoppingCart.png';
 
-const DraggableCart = props => (
-  <View style={styles.box}>
-    <Image source={shoppingCart} style={{ width: 26, height: 26 }} />
-  </View>
-);
+export default class DraggableCart extends Component {
+
+  constructor(props) {
+    super(props);
+    this._translateX = new Animated.Value(0);
+    this._translateY = new Animated.Value(0);
+    this._lastOffset = { x: 0, y: 0 };
+    this._onGestureEvent = Animated.event(
+      [
+        {
+          nativeEvent: {
+            translationX: this._translateX,
+            translationY: this._translateY,
+          },
+        },
+      ],
+      {
+        useNativeDriver: true,
+      }
+    );
+  }
+
+  _onHandlerStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      this._lastOffset.x += event.nativeEvent.translationX;
+      this._lastOffset.y += event.nativeEvent.translationY;
+      this._translateX.setOffset(this._lastOffset.x);
+      this._translateX.setValue(0);
+      this._translateY.setOffset(this._lastOffset.y);
+      this._translateY.setValue(0);
+    }
+  };
+
+  render() {
+    return(
+      <PanGestureHandler {...this.props} onGestureEvent={this._onGestureEvent} onHandlerStateChange={this._onHandlerStateChange}>
+        <Animated.View
+          style={[
+            styles.box,
+            {
+              transform: [{ translateX: this._translateX }, { translateY: this._translateY }],
+            },
+            this.props.boxStyle,
+          ]}
+        >
+          <Image source={shoppingCart} style={{ width: 26, height: 26 }} />
+        </Animated.View>
+      </PanGestureHandler>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   box: {
